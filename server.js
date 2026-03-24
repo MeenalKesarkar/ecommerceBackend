5ad1eb6a16ecdbe33f61d786720155d42a484707
Meenal Kesarkar <159876692+MeenalKesarkar@users.noreply.github.com>
2026-03-24 14:08:34 +0530

    poer problem solved

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use('/api/payment-summary', paymentSummaryRoutes);
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Catch-all route to serve index.html for any unmatched routes
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   const indexPath = path.join(__dirname, 'dist', 'index.html');
   if (fs.existsSync(indexPath)) {
     res.sendFile(indexPath);
